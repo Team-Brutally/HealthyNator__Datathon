70171df53b0a8e690e6cf85e56490f93c4de8f32
MinavKaria <minavpkaria@gmail.com>
2024-02-03 13:01:50 +0530

    Add patient schema

--- server/models/patientUser.js
+++ server/models/patientUser.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
 const patientSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    id: { type: String },
-    password: { type: String, required: true },
-    hospital: { type: String, required: true },
-    medicalHistory: { type: String }
+    age: {type: Number,required},
+    gender: {type: String,required:true},
+    height: {type: Number,required:true},
+    weight: {type: Number,required:true},
+    contactno: {type: Number,required:true},
+    address: {type: String,required:true},
+    bloodgroup: {type: String,required:true},
+    allergies: [String],
+    medicalhistory: [String],
+    currentmedication: [String],
+    familymedicalhistory: [String],
+    email: {type: String,required:true}, 
+    id: {type: String,required:true},
+    createdAt: {type: Date,default: new Date()}
 });
 
 const Patient = mongoose.model('Patient', patientSchema);
