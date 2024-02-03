df7e4e79f6cf782948a567cf6eaa1448c8da35ac
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-02-03 16:13:22 +0530

    Added Email services

--- server/models/patientUser.js
+++ server/models/patientUser.js
@@ -1,21 +1,20 @@
 import mongoose from "mongoose";
 
 const patientSchema = mongoose.Schema({
-    age: {type: Number,required:true},
-    gender: {type: String,required:true},
-    height: {type: Number,required:true},
-    weight: {type: Number,required:true},
-    contactno: {type: Number,required:true},
-    address: {type: String,required:true},
-    bloodgroup: {type: String,required:true},
-    allergies: [String],
-    medicalhistory: [String],
-    currentmedication: [String],
-    familymedicalhistory: [String],
-    email: {type: String,required:true}, 
-    id: {type: String,required:true},
-    createdAt: {type: Date,default: new Date()}
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  height: { type: Number, required: true },
+  weight: { type: Number, required: true },
+  contactno: { type: Number, required: true },
+  address: { type: String, required: true },
+  bloodgroup: { type: String, required: true },
+  allergies: [String],
+  medicalhistory: [String],
+  currentmedication: [String],
+  familymedicalhistory: [String],
+  email: { type: String, required: true },
+  id: { type: String, required: true },
+  createdAt: { type: Date, default: new Date() },
 });
 
-export default mongoose.model('Patient', patientSchema);
-
+export default mongoose.model("Patient", patientSchema);
