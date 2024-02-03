449a5465129b9bb9baecd53a054f244b8bfda5a4
atharvaupare <atharvaupare5@gmail.com>
2024-02-03 20:38:02 +0530

    feat: ai page

---
+++ client/src/components/Chip.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const Chip = ({ symptom, isActive, onClick }) => {
+  const [active, setActive] = useState(isActive);
+
+  const handleActive = () => {
+    setActive(!active);
+    if (onClick) {
+      onClick(!active); // Call the onClick prop with the updated active state
+    }
+  };
+
+  return (
+    <div
+      className={`h-fit w-fit  px-4 py-2 rounded-full border flex gap-x-2 items-center justify-center ${
+        active ? "border-black bg-[#fff] text-black" : " border-white bg-[#222]"
+      }`}
+      onClick={handleActive}
+    >
+      <p>{symptom}</p>
+      {active && (
+        <img className="h-5" src={require("../assets/closecircle2.svg")}></img>
+      )}
+    </div>
+  );
+};
+
+export default Chip;
