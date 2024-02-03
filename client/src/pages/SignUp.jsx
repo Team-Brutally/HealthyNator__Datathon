5877cdb400ecd9a5e660218112ec3c490ccb06b2
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-03 17:43:21 +0530

    Formatted better

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,27 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 import "../css/SignUp.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const bloodgroups = [
-  {value: 'A+',label: 'A+',},{value: 'A-',label: 'A-',},{value: 'B+',label: 'B+',},{value: 'B-',label: 'B-',},{value: 'AB+',label: 'AB+',},{value: 'AB-',label: 'AB-',},{value: 'O+',label: 'O+',},{value: 'O-',label: 'O-',},
+  { value: "A+", label: "A+" },
+  { value: "A-", label: "A-" },
+  { value: "B+", label: "B+" },
+  { value: "B-", label: "B-" },
+  { value: "AB+", label: "AB+" },
+  { value: "AB-", label: "AB-" },
+  { value: "O+", label: "O+" },
+  { value: "O-", label: "O-" },
 ];
 
 const genders = [
-  {value: "male", label: "Male"}, {value: "Female", label: "Female"}
-]
+  { value: "male", label: "Male" },
+  { value: "Female", label: "Female" },
+];
 function SignUp() {
   const textFieldInputProps = {
     style: {
@@ -28,72 +36,142 @@ function SignUp() {
       color: "white", // Set the label text color to white
     },
   };
-  return (  
+  return (
     <>
       <div className="masterSignUp">
         <div className="suCont">
           <span>Please enter the details...</span>
-          <span><TextField variant="filled" label="Name: " placeholder="Name"
-          InputProps={textFieldInputProps}
-          InputLabelProps={textFieldInputLabelProps}/></span>
-          <span><TextField variant="filled" label="Age: " placeholder="Age"
-          InputProps={textFieldInputProps}
-          InputLabelProps={textFieldInputLabelProps}/></span>
-          <span><TextField label="Email: " placeholder="Email"
-          InputProps={textFieldInputProps}
-          InputLabelProps={textFieldInputLabelProps}/></span>
-          <span><TextField variant="filled" label="Contact: " placeholder="Contact"
-          InputProps={textFieldInputProps}
-          InputLabelProps={textFieldInputLabelProps}/></span>
-          <span><TextField variant="filled" 
-          InputProps={textFieldInputProps}
-          InputLabelProps={textFieldInputLabelProps}
-          select
-          label="Please select your Blood group"
-          defaultValue="A+"
-          fullWidth
-        >
-          {bloodgroups.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField></span>
-        <span><TextField variant="filled" 
-        InputProps={textFieldInputProps}
-        InputLabelProps={textFieldInputLabelProps}
-          select
-          label="Please select your gender"
-          fullWidth
-          defaultValue="male"
-          helperText=""
-        >
-          {genders.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField></span>
-            <span><TextField variant="filled" label="Height-Feet"
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps}></TextField><TextField label="Inches"></TextField></span>
-            <span><TextField variant="filled" label="Weight-kg"
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps}></TextField></span>
-            <span><TextField variant="filled" label="Any Previous allergies?" 
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps} fullWidth></TextField></span>
-            <span><TextField variant="filled" label="Your address" fullWidth
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps}></TextField></span>
-            <span><TextField variant="filled" label="Password" fullWidth
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps}></TextField></span>
-            <span><TextField variant="filled" label="Confirm Password" fullWidth
-            InputProps={textFieldInputProps}
-            InputLabelProps={textFieldInputLabelProps}></TextField></span>
-            <span><Button variant="contained" style={{ backgroundColor: '#4461F2', color: 'white' }}>Sign Up</Button></span>
-            
+          <span>
+            <TextField
+              variant="filled"
+              label="Name: "
+              placeholder="Name"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            />
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Age: "
+              placeholder="Age"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            />
+          </span>
+          <span>
+            <TextField
+              label="Email: "
+              placeholder="Email"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            />
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Contact: "
+              placeholder="Contact"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            />
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+              select
+              label="Please select your Blood group"
+              defaultValue="A+"
+              fullWidth
+            >
+              {bloodgroups.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+              select
+              label="Please select your gender"
+              fullWidth
+              defaultValue="male"
+              helperText=""
+            >
+              {genders.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Height-Feet"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            ></TextField>
+            <TextField label="Inches"></TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Weight-kg"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            ></TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Any Previous allergies?"
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+              fullWidth
+            ></TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Your address"
+              fullWidth
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            ></TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Password"
+              fullWidth
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            ></TextField>
+          </span>
+          <span>
+            <TextField
+              variant="filled"
+              label="Confirm Password"
+              fullWidth
+              InputProps={textFieldInputProps}
+              InputLabelProps={textFieldInputLabelProps}
+            ></TextField>
+          </span>
+          <span>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#4461F2", color: "white" }}
+            >
+              Sign Up
+            </Button>
+          </span>
         </div>
       </div>
     </>
