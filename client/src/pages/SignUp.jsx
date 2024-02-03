73f9283927579774e704b3ff7ab0f4bef1c31bbf
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-03 16:46:10 +0530

    Created Sign Up form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 import "../css/SignUp.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+const bloodgroups = [
+  {value: 'A+',label: 'A+',},{value: 'A-',label: 'A-',},{value: 'B+',label: 'B+',},{value: 'B-',label: 'B-',},{value: 'AB+',label: 'AB+',},{value: 'AB-',label: 'AB-',},{value: 'O+',label: 'O+',},{value: 'O-',label: 'O-',},
+];
+
+const genders = [
+  {value: "male", label: "Male"}, {value: "Female", label: "Female"}
+]
 function SignUp() {
   return (
     
@@ -11,28 +22,41 @@ function SignUp() {
       <div className="masterSignUp">
         <div className="suCont">
           <span>Please enter the details...</span>
-          <span><input type="text" value="name" placeholder="Name"></input></span>
-          <span><input type="text" value="age" placeholder="Age"></input></span>
-          <span><input type="text" value="email" placeholder="Email"></input></span>
-          <span><label>BloodGroup: </label><select name="blood">
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="O+">O+</option>
-            <option value="A-">O-</option>
-            <option value="AB">AB</option>
-            </select></span>
-            <span><label>Gender:</label><select name="gender">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              </select></span>
-            <span><input type="text" value="contact" placeholder="Contact No."></input></span>
-            <span><label>Height: </label><input type="Number"></input><label>'</label><input type="number"></input><label>''</label></span>
-            <span><label>Weight</label><input type="Number"></input><label>kg</label></span>
-            <span><input type="text" name="alergies" placeholder="Any previous allergies"></input></span>
-            <span><input type="text" name="address" placeholder="Address"></input></span>
-            <span><button>Sign Up</button></span>
+          <span><TextField label="Name: " placeholder="Name"/></span>
+          <span><TextField label="Age: " placeholder="Age"/></span>
+          <span><TextField label="Email: " placeholder="Email"/></span>
+          <span><TextField label="Contact: " placeholder="Contact"/></span>
+          <span><TextField
+          select
+          label="Please select your Blood group"
+          defaultValue="A+"
+          fullWidth
+        >
+          {bloodgroups.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField></span>
+        <span><TextField
+          select
+          label="Please select your gender"
+          fullWidth
+          defaultValue="male"
+          helperText=""
+        >
+          {genders.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField></span>
+            <span><TextField label="Height-Feet"></TextField><TextField label="Inches"></TextField></span>
+            <span><TextField label="Weight-kg"></TextField></span>
+            <span><TextField label="Any Previous allergies?" fullWidth></TextField></span>
+            <span><TextField label="Your address" fullWidth></TextField></span>
+            <span><Button variant="contained" style={{ backgroundColor: '#4461F2', color: 'white' }}>Sign Up</Button></span>
+            
         </div>
       </div>
     </>
